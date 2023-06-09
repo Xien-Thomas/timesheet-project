0eb5fa805ab584f0b8d4765b72cd3eadaa50c9e4
TrashPandaRX <efvalerius@gmail.com>
2023-06-09 10:49:33 -0500

    started ViewController user-story
    started ViewContractor user-story

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -11,6 +11,16 @@ class UserController < ApplicationController
   def show
   end
 
+  # this is the basis of "View Contractors", this will view contractors UNDER the vendor in question
+  # input is just be the vendor's info
+  # output is just the contractor(s) under the given vendor
   def index
+    # T - we're going to grab the vendor info from the url (ie that will be our params)
+    params = @requests.params
+    vendor = params[:vendor]
+
+    contractors = User.where("vendor = ?", vendor)
+
+    return json: contractors.to_json
   end
 end
