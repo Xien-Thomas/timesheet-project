1a1b2aa8489079b9fa0f3cc4f95b34cfc05505dd
Xien Thomas <xien.thomas@gmail.com>
2023-06-09 12:56:25 -0500

    [M] started added tests for index user controller

--- test/controllers/user_controller_test.rb
+++ test/controllers/user_controller_test.rb
@@ -1,5 +1,20 @@
 require "test_helper"
 
 class UserControllerTest < ActionDispatch::IntegrationTest
-  
+    setup do
+        @user1 = users(:user1)
+        @user1_token = JsonWebToken.encode('user_id' => @user1.id)
+        @user1_Auth = {"Autherization" => "Bearer #{@user1_token}"}
+
+        @manager1 =users(:manager)
+        @manager1_token = JsonWebToken.encode('user_id' => @manager1.id)
+    end
+
+    test 'should be true' do
+        assert true
+    end
+
+    test 'Manager should be able to get contractors' do
+        
+    end
 end
