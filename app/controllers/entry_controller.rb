33336eae730009965074c0aaa151a3851350d9a5
Trish1996-wq <90051133+Trish1996-wq@users.noreply.github.com>
2023-07-21 16:40:28 -0400

    entry#destroy test and controller

--- app/controllers/entry_controller.rb
+++ app/controllers/entry_controller.rb
@@ -1,36 +1,13 @@
 class EntryController < ApiBaseController
 
-  def create
-    # return reject_unauthorized_request unless @current_user.employee?
-
-    # @entry = Entry.new entry_params
-
-    # if @entry.save
-    #   return render json: { message: 'Entry successfully created' }, status: :created
-    # else
-    #   return render json: { message: @entry.errors.full_messages.join(", ") }, status: :unprocessable_entity
-    # end
-  end
-
   def destroy
+    return reject_unauthorized_request if @current_user.employee?
 
+    entry_to_destroy = Entry.find params[:id]
+    if entry_to_destroy.destroy
+      return render json: { message: "Entry successfully deleted."}, status: :ok
+    else
+      return render json: { message: entry_to_destroy.errors.full_messages.join(', ') }, status: 500 
+    end
   end
-
-  def update
-
-  end
-
-  def index
-
-  end
-
-  # private
-
-  # def entry_params
-  #   params.require(:entry).permit(
-  #     :date, 
-  #     :hours,
-  #     :entry_type,
-  #     :timesheet_id)
-  # end
 end
